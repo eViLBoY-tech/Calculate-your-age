aede27d5e972671ff8869c729fc22c6fda0afa7b
EviLBoY <67540754+eViLBoY-tech@users.noreply.github.com>
2020-06-28 14:44:30 +0430

    Create run.py

---
+++ run.py
@@ -0,0 +1,8 @@
+#Created By eViLBoY
+#for fun
+#--------------------
+print ("Please enter your year of birth")
+years = input ()
+test = int(years) -2020
+print (f"Your were born in {years} And your age {-test}")
+print ("Done.")
